a84bba4a74a052289d34e875a5b6db30d86b52b7
HeZhijun4030 <Zhijun4030@outlook.com>
2025-09-19 22:59:33 +0800

    [Playing] SPDT Stage updated

--- assets/shared/stages/hall.lua
+++ assets/shared/stages/hall.lua
@@ -1,7 +1,7 @@
 function onCreate()
 	-- background shit
 
-	makeLuaSprite('hall', 'hall', -500, 20);
+	makeLuaSprite('hall', 'hall', -500, 50);
 	setLuaSpriteScrollFactor('hall', 0.9, 0.9);
 	scaleObject('hall', 0.9, 0.9);
                 addLuaSprite('hall',false)
