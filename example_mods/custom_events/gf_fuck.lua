4b60f65085a069bcb82f123a5e976e4b9d1fdcf2
HeZhijun4030 <Zhijun4030@outlook.com>
2025-10-04 23:12:28 +0800

    [hotfix]fixed event

--- example_mods/custom_events/gf_fuck.lua
+++ example_mods/custom_events/gf_fuck.lua
@@ -1,10 +1,10 @@
 
 function onEvent(name,value1,value2)
 if name == 'gf_fuck' then
-    if value1== 1 then
+    if value1== '1' then
         setProperty('gfGroup.visible', false)
     end
-    if value1== 2 then
+    if value1== '2' then
         setProperty('gfGroup.visible', true)
     end
 	end
